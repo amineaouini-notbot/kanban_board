14e2a4e4d599dd364e2dd86983e025fac7acb4e2
amine m. aouini <aminaouini565@gmail.com>
2022-05-02 22:42:43 +0100

    setup kanban board redux slice

---
+++ client/src/kanbanSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const kanbanSlice = createSlice({
+    name: 'kanban',
+    initialState: {},
+    reducers: {
+
+    }
+})
+
+export default kanbanSlice.reducer;
