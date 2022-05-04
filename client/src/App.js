42298e0b7f4c1b5124c448280187b9371cc8e4eb
amine m. aouini <aminaouini565@gmail.com>
2022-05-04 01:58:56 +0100

    setup react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <div className="App">
+        <h1>kanban Board</h1>
+        <Routes>
+          <Route path='/' element={<h1>1111</h1>} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
