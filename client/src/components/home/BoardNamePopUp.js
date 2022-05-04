edcf8e4b8df9c5c26f30e86673b25c6ca86ef130
amine m. aouini <aminaouini565@gmail.com>
2022-05-05 00:40:55 +0100

    create board in popup done

--- client/src/components/home/BoardNamePopUp.js
+++ client/src/components/home/BoardNamePopUp.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './popup.css'
+import { addBoard } from "../../kanbanSlice";
+import { useDispatch } from "react-redux";
 
 const BNPopUp = (props) => {
+    const dispatch = useDispatch();
+    const [kanbanName, setName] = useState('');
+    const handleChange = (e) => setName(e.target.value)
     return (
         <div className='popup'>
             <div className='popup_inner'>
                 <button id="close_popup" onClick={props.closePopup}>x</button>
                 <form>
-                    <input placeholder="Write you board name"></input>
-                    <button>Create</button>
+                    <input onChange={handleChange} placeholder="Write you board name"></input>
+                    <button onClick={() => dispatch(addBoard({ name: kanbanName }))}>Create</button>
                 </form>
             </div>
         </div>
