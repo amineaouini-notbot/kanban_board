89abb1b5ff73d1c1810c61c51e5622006553bacf
amine m. aouini <aminaouini565@gmail.com>
2022-05-06 01:06:51 +0100

    display board once created

---
+++ client/src/components/home/Boards.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Boards = (props) => {
+    // const redirect = props.redirect(props.id)
+    return (
+        <div >
+            <p>{props.boardName}</p>
+        </div>
+    )
+}
+
+export default Boards;
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import BNPopUp from "./BoardNamePopUp";
 import './home.css';
-
+import Boards from "./Boards";
 const Home = () => {
     let boards = useSelector(state => state.kanban.boards);
     const [showPopUp, togglePopup] = useState(false);
     let togglePopUp = () => togglePopup(!showPopUp);
     return (
         <div>
-            <div className="content" id="create_board" onClick={togglePopUp}><p>Create a new Board +</p></div>
+            <div id="content">
+                {boards.map((board, i) => <Boards boardName={board.name} id={i} />)}
+                <div id="create_board" onClick={togglePopUp}><p>Create a new Board +</p></div>
+
+            </div>
             {showPopUp ? <BNPopUp closePopup={togglePopUp} /> : null}
         </div>
     )
