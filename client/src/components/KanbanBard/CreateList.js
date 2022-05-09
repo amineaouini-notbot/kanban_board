91cf80000146f8211f73d9501780e0fdd58a48dc
amine m. aouini <aminaouini565@gmail.com>
2022-05-09 13:32:36 +0100

    removeadd list input once out of focus

--- client/src/components/KanbanBard/CreateList.js
+++ client/src/components/KanbanBard/CreateList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { addList } from "../../kanbanSlice";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -13,6 +13,16 @@ const CreateList = (props) => {
             setOnCreate(false)
         }
     }
+    let addListRef = useRef(null);
+    useEffect(() => {
+        let handler = event => {
+            if (!addListRef.current.contains(event.target)) {
+                setOnCreate(false)
+            }
+        }
+        document.addEventListener('mousedown', handler)
+        return () => { document.removeEventListener('mousedown', handler) }
+    })
     return !onCreate ? (<div onClick={() => setOnCreate(true)}>Add List +</div>) :
         <div>
             <input onChange={(e) => setListName(e.target.value)}></input>
