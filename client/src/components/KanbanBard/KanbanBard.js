79538dc516dba8d2f5b3c7550064ff2c5da4dab2
amine m. aouini <aminaouini565@gmail.com>
2022-05-09 11:44:20 +0100

    redirect to board one created

--- client/src/components/KanbanBard/KanbanBard.js
+++ client/src/components/KanbanBard/KanbanBard.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const KanbanBoard = () => {
     let { id } = useParams();
-    return (<div> Hi {id}</div>)
+    let board = useSelector(state => state.kanban.boards[id]);
+    let { name, lists } = board;
+    return (
+        <div>
+            <h2>{name}</h2>
+        </div>
+    )
 }
 
 export default KanbanBoard;
