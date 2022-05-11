5bad1bc2be3cb22ae9fc8f3859505a5d04903719
amine m. aouini <aminaouini565@gmail.com>
2022-05-11 22:29:06 +0100

    betteradd list style

--- client/src/components/KanbanBard/CreateList.js
+++ client/src/components/KanbanBard/CreateList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import { addList } from "../../kanbanSlice";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import './CreateList.css';
+
 const CreateList = (props) => {
     const { id } = useParams();
     const dispatch = useDispatch();
@@ -23,10 +25,10 @@ const CreateList = (props) => {
         document.addEventListener('mousedown', handler)
         return () => { document.removeEventListener('mousedown', handler) }
     })
-    return !onCreate ? (<div onClick={() => setOnCreate(true)}>Add List +</div>) :
-        <div ref={addListRef}>
+    return !onCreate ? (<div id="add_list" onClick={() => setOnCreate(true)}><span>+ Add a list</span></div>) :
+        <div id="on_add_list" ref={addListRef}>
             <input onChange={(e) => setListName(e.target.value)}></input>
-            <button onClick={handleCreate}>Add +</button>
+            <button onClick={handleCreate}>Add list</button>
         </div>
 }
 
