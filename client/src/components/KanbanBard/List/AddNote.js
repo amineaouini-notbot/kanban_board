0259d2ba0effeae90476370af160b57fe250ed58
amine m. aouini <aminaouini565@gmail.com>
2022-05-18 20:44:40 +0100

    set up functionality

--- client/src/components/KanbanBard/List/AddNote.js
+++ client/src/components/KanbanBard/List/AddNote.js
@@ -3,21 +3,25 @@ import { useDispatch } from 'react-redux';
 import { addNote } from '../../../kanbanSlice';
 import './AddNote.css';
 
-const AddNote = () => {
+const AddNote = (props) => {
     const dispatch = useDispatch();
     const [onAdd, setOnAdd] = useState(false);
     const [note, setNote] = useState('')
-    let handelOnAdd = () => setOnAdd(!onAdd);
+    const { BIndex, LIndex } = props;
+    let handleOnAdd = () => {
+        dispatch(addNote({ BIndex, LIndex, note }));
+        setOnAdd(false)
+    };
 
     return (
         <div className='add_note_container'>
-            {!onAdd ? <div className='add_note' onClick={handelOnAdd}><div className='add_note_text'><span>+</span> Add a Note</div></div> :
+            {!onAdd ? <div className='add_note' onClick={() => setOnAdd(true)}><div className='add_note_text'><span>+</span> Add a Note</div></div> :
                 <div className='on_add_note'>
                     <textarea onChange={e => setNote(e.target.value)}></textarea>
                     <div className='add_note_actions'>
 
-                        <button>Add note</button>
-                        <span onClick={handelOnAdd}>x</span>
+                        <button onClick={handleOnAdd}>Add note</button>
+                        <span onClick={() => setOnAdd(false)}>x</span>
                     </div>
                 </div>}
         </div>
