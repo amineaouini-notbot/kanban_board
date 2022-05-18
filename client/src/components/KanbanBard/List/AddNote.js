bd0a7aeaeb10420d1db11920e0dd2dceda74e490
amine m. aouini <aminaouini565@gmail.com>
2022-05-18 01:29:35 +0100

    setup on add note

--- client/src/components/KanbanBard/List/AddNote.js
+++ client/src/components/KanbanBard/List/AddNote.js
@@ -6,11 +6,19 @@ import './AddNote.css';
 const AddNote = () => {
     const dispatch = useDispatch();
     const [onAdd, setOnAdd] = useState(false);
+    const [note, setNote] = useState('')
     let handelOnAdd = () => setOnAdd(!onAdd);
 
     return (
         <div className='add_note_container'>
-            {!onAdd ? <div className='add_note' onClick={handelOnAdd}><div className='add_note_text'><span>+</span> Add a Note</div></div> : <div className='on_add_note'>on add</div>}
+            {!onAdd ? <div className='add_note' onClick={handelOnAdd}><div className='add_note_text'><span>+</span> Add a Note</div></div> :
+                <div className='on_add_note'>
+                    <div className='add_note_actions'>
+
+                        <textarea onChange={e => setNote(e.target.value)}></textarea>
+                        <span>Add</span><span onClick={handelOnAdd}>x</span>
+                    </div>
+                </div>}
         </div>
     )
 }
