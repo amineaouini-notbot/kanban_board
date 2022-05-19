2ba5f8d9684a4e5f9a19729deffcd4aa22d3474c
amine m. aouini <aminaouini565@gmail.com>
2022-05-20 00:32:28 +0100

    setup bottom border shadow in note

--- client/src/components/KanbanBard/List/Note.js
+++ client/src/components/KanbanBard/List/Note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './Note.css';
 
@@ -6,9 +6,13 @@ const Note = (props) => {
     const { NIndex, LIndex, BIndex } = props;
     // @ts-ignore
     const note = useSelector(state => state.kanban.boards[BIndex].lists[LIndex].content[NIndex]);
+    const [content, setContent] = useState(note);
+
     return (
-        <div className='note'>
-            {note}
+        <div className='note_container'>
+            <span>
+                {content}
+            </span>
         </div>
     )
 }
