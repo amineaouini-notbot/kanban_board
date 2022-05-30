a385e8ea39f5c77f1a554ef17c21dc8b5d82a5ec
amine m. aouini <aminaouini565@gmail.com>
2022-05-30 21:38:51 +0100

    setup draggable notes

--- client/src/components/KanbanBard/List/Note.js
+++ client/src/components/KanbanBard/List/Note.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './Note.css';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Note = (props) => {
     const { NIndex, LIndex, BIndex } = props;
@@ -8,11 +9,16 @@ const Note = (props) => {
     const note = useSelector(state => state.kanban.boards[BIndex].lists[LIndex].content[NIndex]);
 
     return (
-        <div className='note_container'>
-            <span className='note'>
-                {note}
-            </span>
-        </div>
+        <Draggable>
+            {provider => (
+
+                <div className='note_container' ref={provider.innerRef}>
+                    <span className='note'>
+                        {note}
+                    </span>
+                </div>
+            )}
+        </Draggable>
     )
 }
 
