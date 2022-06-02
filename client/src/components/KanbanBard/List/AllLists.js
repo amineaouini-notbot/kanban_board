1cd12af28acbb22c23530ac6eeee4d5e2bd06fe3
amine m. aouini <aminaouini565@gmail.com>
2022-06-02 13:53:18 +0100

    solved draging list problem

--- client/src/components/KanbanBard/List/AllLists.js
+++ client/src/components/KanbanBard/List/AllLists.js
@@ -10,29 +10,33 @@ const AllLists = (props) => {
     let lists = useSelector(state => state.kanban.boards[BIndex].lists)
     const handleNoteDrag = result => {
         if (!result.destination) return
-        let payload = {
-            BIndex: BIndex,
-            s_id: result.source.droppableId,
-            d_id: result.destination.droppableId,
-            s_index: result.source.index,
-            d_index: result.destination.index
-        }
-        dispatch(reorderedNotes(payload))
+        if (result.source.droppableId === 'all_lists') {
 
+            let payload = {
+                BIndex,
+                DIndex: result.destination.index,
+                SIndex: result.source.index
+            }
+            dispatch(reorderLists(payload))
+        }
+        else {
+            let payload = {
+                BIndex: BIndex,
+                s_id: result.source.droppableId,
+                d_id: result.destination.droppableId,
+                s_index: result.source.index,
+                d_index: result.destination.index
+            }
+            dispatch(reorderedNotes(payload))
+        }
     }
 
     const handleOnDrag = result => {
         if (!result.destination) return
-        let payload = {
-            BIndex,
-            DIndex: result.destination.index,
-            SIndex: result.source.index
-        }
-        dispatch(reorderLists(payload))
     }
     return (
         <div id="content">
-            <DragDropContext onDragEnd={handleOnDrag}>
+            <DragDropContext onDragEnd={handleNoteDrag}>
                 <Droppable droppableId="all_lists" direction="horizantal" type="LIST">
                     {(provided) => {
                         return (
