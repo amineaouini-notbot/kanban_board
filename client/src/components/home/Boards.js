6170be1a787b8a3a88e2391f8b05be64f5a97e1a
amine m. aouini <aminaouini565@gmail.com>
2023-03-11 20:54:57 +0100

    removed disclamer

--- client/src/components/home/Boards.js
+++ client/src/components/home/Boards.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './Boards.css';
-
+import { Link } from "react-router-dom";
 const Boards = (props) => {
     // const redirect = ()=> props.redirect(props.id)
     return (
-        <div className="board">
-            <p>{props.boardName}</p>
+        <div>
+
+        <Link id="boards" to={`/board/` + props.id}>
+            <div className="board">
+                <p>{props.boardName}</p>
+            </div>
+
+        </Link >
         </div>
     )
 }
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import BNPopUp from "./BoardNamePopUp";
 import './home.css';
 import Boards from "./Boards";
+
 const Home = () => {
     let boards = useSelector(state => state.kanban.boards);
     const [showPopUp, togglePopup] = useState(false);
@@ -10,26 +11,14 @@ const Home = () => {
     return (
         <div>
             <h1 id="title">kanban Board</h1>
-            <div id="content" style={boards.length === 2 ? { griTemplateColumns: 'auto auto', marginLeft: '15%' } : {}}>
-                {boards.map((board, i) => <Boards boardName={board.name} id={i} />)}
+            <div id="boards_content">
 
-            </div>
-            <div id="create_board" onClick={togglePopUp}><p>Create a new Board +</p></div>
-            <div id="disclaimer">
-                <div id="disclaimer_content">
-                    <h4>Disclaimer:</h4>
-                    <p>
-                        if you create a board it won’t be saved in a data base.
-                        this website is created by Amine m. aouini a rising front end developer from tunisia looking for internship remotely abroad.</p>
+                <div id="content" style={boards.length === 2 ? { griTemplateColumns: 'auto auto', marginLeft: '15%' } : {}}>
+                    {boards.map((board, i) => <Boards boardName={board.name} id={i} />)}
 
                 </div>
-            </div>
-            <div id="my_info_container">
-                <div id="my_info">
-                    <h4>You want to get in touch ?</h4>
-                    <p>Whatsapp: +21621216971 | LinkedIn: /in/amine-aouini | GitHub: m-amine-aouini</p>
-                </div>
-
+                
+                <div id="create_board" onClick={togglePopUp}><p>Create a new Board +</p></div>
             </div>
             {showPopUp ? <BNPopUp closePopup={togglePopUp} /> : null}
         </div>
