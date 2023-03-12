9723f94c17e092d9ef9ecf6ba072b941670a8ba7
amine m. aouini <aminaouini565@gmail.com>
2023-03-12 16:26:05 +0100

    setup db code structure

---
+++ db/mysqldb.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE kanban_board;
+
+USE kanban_board;
+
+CREATE TABLE boards (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    made_at DATETIME NOT NULL,
+    PRIMARY KEY (id, name, made_at)
+);
+
+CREATE TABLE lists (
+    id INT NOT NULL AUTO_INCREMENT,
+    boards_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (boards_id) REFERENCES boards(id)
+);
+
+CREATE TABLE notes (
+    id INT NOT NULL AUTO_INCREMENT,
+    list_id INT NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (list_id) REFERENCES lists(id)
+);
