5fcf92ed81e9ea6532cc07b4e1fd80e548c0b381
amine m. aouini <aminaouini565@gmail.com>
2023-03-13 16:50:22 +0100

    add documentation to code

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {boards_api} = require('./routes/boards');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/board', boards_api); 
+// any http request starts with /board uses boards.js to handle endpoints
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)});
--- routes/boards.js
+++ routes/boards.js
@@ -1,7 +1,14 @@
-let boards_api = require('express');
-let {db} = require("db/mysqldb.js");
+let boards_api = require('express').Router();
+let {db} = require("../db/mysqldb");
 
 boards_api.post('/create', (req, res)=>{
-    let {name, created_at} = req.body;
-    
+    let {name, made_at} = req.body; // retreive request data
+    db.query(`INSERT INTO customers (name, made_at) VALUES ?`, [name, made_at],  // insert data in the db
+        (err, result)=>{
+            if (err) throw err; // throw err incase any problem shows up
+            console.log(result.insertId);
+
+    })
 })
+
+module.exports = {boards_api}
