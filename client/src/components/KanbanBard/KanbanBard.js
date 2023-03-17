fb15d52a045d7f9d7ee971d46ada3d7d67ec134c
amine m. aouini <aminaouini565@gmail.com>
2023-03-17 22:34:48 +0100

    setup get lists req front

--- client/src/components/KanbanBard/KanbanBard.js
+++ client/src/components/KanbanBard/KanbanBard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
 import './KanbanBoard.css';
 import AllLists from "./List/AllLists";
@@ -8,7 +9,14 @@ const KanbanBoard = () => {
     let lastOpened = openedBoards[openedBoards.length-1]
     console.log(lastOpened)
     let openedBoard = useSelector(state => state.kanban.boards[lastOpened.index]) || 1;
-    
+    let [listsRetrieved, isRetrieved] = useState(false);
+    if (!listsRetrieved){
+        axios.get('/api/lists/retrieve', {BId: openedBoard.id})
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err => {throw err});
+    }
     return openedBoard.name ? (<div>
         <div id="board_title">
             <h2>{openedBoard.name}</h2>
